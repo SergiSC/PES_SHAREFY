d13ac0e578ada4935c635d6b1170ba2ad77e1faf
oriol.maristany <oriol.maristany@est.fib.upc.edu>
2019-10-12 14:52:01 +0200

    #40 password + aletres i crides a butons

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ControlContainer } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -8,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, 
+              public alertController: AlertController) {}
+
+  entrar(){
+    let InUserName = (document.getElementById('input-user-email') as HTMLInputElement).value;
+    let InPassword = (document.getElementById('input-password') as HTMLInputElement).value;
+    console.log(InUserName, InPassword);
+    if (!(0 || InUserName.length === 0 || InPassword.length === 0)) { this.router.navigateByUrl('/tabs');}
+    else { this.presentAlert() }
+    //Enviar les variabes a la base de dades i si funciona return
+  }
+
+  async presentAlert() {
+    let alert = this.alertController.create({
+      header: 'Login Fallit',
+      subHeader: 'El Usuari, email o contrasenya introduit no correspon amb ningun usuari',
+      buttons: ['Okei']
+    });
+    (await alert).present();
+  }
+
 
   ngOnInit() {
+    //Mirar a la base de dades si te el toquen
+    //si el te anar a la seguent pagina
+    if (0) { this.router.navigateByUrl('/tabs');}
+
   }
 
 }
